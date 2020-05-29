119f31da8afb3f97c73459aa5444649faf00d828
P-iknow <apricotsoul@gmail.com>
2020-05-29 23:12:05 +0900

    fix: for in issue

--- code-spitz/77-js-basic/04/compleRecursion.js
+++ code-spitz/77-js-basic/04/compleRecursion.js
@@ -31,18 +31,18 @@ const complexRecursion2 = {
 	data: [{ a: [1, 2, 3, 4], b: '-' }, [5, 6, 7], 8, 9],
 	next() {
 		let v;
-		let temp = [];
 		while ((v = this.data.shift())) {
 			switch (true) {
 				case Array.isArray(v):
 					this.data.unshift(...v);
 					break;
 				case v && typeof v == 'object':
-					for (let k in v) {
-						temp.push(v[k]);
-					}
-					this.data.unshift(...temp);
-					temp = [];
+					// for in을 그냥 쓰면 Object 객체가 가지고 있는 내장 프로퍼티가 함께 딸려나온다.
+					// hasOwnProperty를 써서 검사를 하던지 아니면 Object.values 를 써서 추출한다.
+					//for (let k in v) {
+					//	if (v.hasOwnProperty(k)) temp.push(v[k]);
+					//}
+					this.data.unshift(...Object.values(v));
 					break;
 				default:
 					return { value: v, done: false };
